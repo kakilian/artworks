04ac3e3bb72b124b3223429a79216bffd3cc1c30
kakilian <160593671+kakilian@users.noreply.github.com>
2025-06-03 09:54:39 +0200

    Added Configure DB to use dj-db-url

--- artworks/settings.py
+++ artworks/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -16,7 +17,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 if os.path.exists(os.path.join(BASE_DIR, 'env.py')):
-    import env
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,10 +135,11 @@ WSGI_APPLICATION = 'artworks.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 

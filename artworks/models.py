613ebc873b45e2c40495375299279eeb0b9880cc
kakilian <160593671+kakilian@users.noreply.github.com>
2025-06-03 05:53:03 +0200

    Updated models, Added icons

--- artworks/models.py
+++ artworks/models.py
@@ -13,8 +13,13 @@ class Artwork(models.Model):
     title = models.CharField(max_length=200)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='artwork_images/', blank=True, null=True)
-    dimensions = models.CharField(max_length=100, blank=True, help_text="e.g. 60 x 80 cm")
+    image = models.ImageField(
+        upload_to='artwork_images/',
+        blank=True, null=True)
+    dimensions = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text="e.g. 60 x 80 cm")
     description = models.TextField(blank=True)
 
     def __str__(self):
--- artworks/urls.py
+++ artworks/urls.py
@@ -3,5 +3,5 @@ from .views import artwork_list, artwork_detail
 
 urlpatterns = [
     path('', artwork_list, name='artworks_list'),         # /artworks/
-    path('<int:pk>/', artwork_detail, name='artwork_detail'),  # /artworks/3/
+    path('<int:pk>/', artwork_detail, name='artwork_detail'),  # /artworks/
 ]

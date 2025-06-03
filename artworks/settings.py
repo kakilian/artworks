286ab313c4ffc12f40b745ccce00be8511b89f34
kakilian <160593671+kakilian@users.noreply.github.com>
2025-06-03 10:12:27 +0200

    Fix indentation error in settings.py

--- artworks/settings.py
+++ artworks/settings.py
@@ -17,6 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 if os.path.exists(os.path.join(BASE_DIR, 'env.py')):
+    pass
 
 
 # Quick-start development settings - unsuitable for production

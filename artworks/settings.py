b983776e732586816a66a84115ce4c5e96fcbfe4
kakilian <160593671+kakilian@users.noreply.github.com>
2025-06-19 12:26:46 +0200

    chore: update DEBUG setting to switch automatically for local vs Render

--- artworks/settings.py
+++ artworks/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 load_dotenv()  # to take environment variables from a .env file, if it exists
 
 
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ  # Set DEBUG to False in production
 
 
 STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')

9bc2ebbb0c74ccbc63df28beb9de8e8548a44a52
kakilian <160593671+kakilian@users.noreply.github.com>
2025-06-21 10:10:48 +0200

    fix: resolved bug issue in urls

--- artworks/urls_project.py
+++ artworks/urls_project.py
@@ -8,7 +8,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('about/', TemplateView.as_view(
+        template_name='about.html',
+        ), name='about'),
     path('accounts/', include('allauth.urls')),  # Include allauth URLs for authentication
     path(
         'artworks/',

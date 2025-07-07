039da98c51cbf57582c9bfb4989becc76f3bb58e
kakilian <160593671+kakilian@users.noreply.github.com>
2025-07-07 08:54:28 +0200

    feat: implement newsletter signup feature (models, forms, views, templates)

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .forms import SubscriberForm
 
-# Create your views here.
+
 def subscribe(request):
+    subscribed = False
     if request.method == 'POST':
         form = SubscriberForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('newsletter:thank_you')
-    return redirect('home')
-    
+            subscribed = True
+            request.session['newsletter_subscribed'] = True
+    else:
+        form = SubscriberForm()
+    return render(request, 'newsletter/signup.html', {
+        'form': form,
+        'subscribed': subscribed,
+    })
+
+
 def thank_you(request):
     return render(request, 'newsletter/thank_you.html')
+
